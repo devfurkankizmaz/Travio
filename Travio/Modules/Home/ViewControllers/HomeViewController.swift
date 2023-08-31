d2b23caa7ee2499fa937fd290dd95a16f8f04eef
devfurkankizmaz <dev.furkan@outlook.com>
2023-08-31 14:55:55 +0300

    home vc added

--- Travio/Modules/Home/ViewControllers/HomeViewController.swift
+++ Travio/Modules/Home/ViewControllers/HomeViewController.swift
@@ -8,10 +8,36 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    // MARK: - Properties
+    
+    private lazy var titleImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "home-logo")
+        return imageView
+    }()
+    
+    private lazy var componentsView = ComponentsView()
+
+    // MARK: - Lifecycle Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupView()
     }
-
+    
+    // MARK: - Private Methods
+    
+    private func setupView() {
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        view.backgroundColor = AppColor.primary.color
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        
+    }
+    
+    // MARK: - Public Methods
+    
+    // MARK: - Actions Methods
 }
