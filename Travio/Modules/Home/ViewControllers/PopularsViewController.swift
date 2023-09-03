f964a0adb6b3da8f3535197b666bd9bb28c3040e
devfurkankizmaz <dev.furkan@outlook.com>
2023-09-03 20:49:54 +0300

    popular vc components added

---
+++ Travio/Modules/Home/ViewControllers/PopularsViewController.swift
@@ -0,0 +1,70 @@
+import SnapKit
+import UIKit
+
+class PopularsViewController: UIViewController {
+    // MARK: - Properties
+
+    private lazy var backButton: UIButton = {
+        let button = UIButton()
+        let arrowImage = UIImage(named: "back")?.withTintColor(.white, renderingMode: .alwaysOriginal)
+        button.setImage(arrowImage, for: .normal)
+        button.contentMode = .scaleAspectFit
+        button.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        return button
+    }()
+
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = AppFont.poppinsSemiBold.withSize(32)
+        label.text = "Popular Places"
+        label.textColor = .white
+        return label
+    }()
+
+    private lazy var componentsView = ComponentsView()
+
+    // MARK: - Lifecycle Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+    }
+
+    // MARK: - Private Methods
+
+    func setupView() {
+        navigationController?.isNavigationBarHidden = true
+        view.addSubviews(componentsView, backButton, titleLabel)
+        view.backgroundColor = AppColor.primary.color
+        setupLayout()
+    }
+
+    func setupLayout() {
+        backButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(24)
+            make.leading.equalToSuperview().offset(24)
+            make.width.height.equalTo(32)
+        }
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(backButton.snp.top).offset(-6)
+            make.leading.equalTo(backButton.snp.trailing).offset(24)
+        }
+        componentsView.snp.makeConstraints { make in
+            make.bottom.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(125)
+        }
+    }
+
+    // MARK: - Public Methods
+
+    // MARK: - Actions
+
+    @objc func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: - Extensions
