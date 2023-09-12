db0dfe68e6dc6c375559cba20c6bdf2c6a9d371e
devfurkankizmaz <dev.furkan@outlook.com>
2023-09-12 11:43:19 +0300

    appdefault settings section deletion

--- Travio/Modules/Menu/Settings/ViewModels/SettingsViewModel.swift
+++ Travio/Modules/Menu/Settings/ViewModels/SettingsViewModel.swift
@@ -13,7 +13,6 @@ class SettingsViewModel {
 
     private let settingsItems: [SettingsItem] = [
         SettingsItem(title: "Security Settings", image: UIImage(named: "securitySettings")!),
-        SettingsItem(title: "App Defaults", image: UIImage(named: "appDefaults")!),
         SettingsItem(title: "My Added Places", image: UIImage(named: "map")!),
         SettingsItem(title: "Help & Support", image: UIImage(named: "helpSupport")!),
         SettingsItem(title: "About", image: UIImage(named: "about")!),
